b45201b6ae9a0feecc7564c9f4728398fd828175
eightman <eightman124@gmail.com>
2025-05-29 10:03:12 +0900

    qwertyuio

--- views/design_view.py
+++ views/design_view.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QFo
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor, QPalette
 from utils.path_utils import get_data_dir
+from utils.ship_type_mapping import ship_type_mapping
+
 
 class DesignView(QWidget):
     def __init__(self, parent=None, app_controller=None, hull_data=None):
@@ -50,104 +52,7 @@ class DesignView(QWidget):
         self.ship_type_combo.addItem("選択してください")
         
         # 艦種マッピングの定義
-        ship_type_mapping = {
-            # 掃海艦艇
-            "AM": "AM - 掃海艇",
-            "CMC": "CMC - 沿岸敷設艇",
-            "MCM": "MCM - 掃海艦",
-            "MCS": "MCS - 掃海母艦",
 
-            # 空母系
-            "AV": "AV - 水上機母艦",
-            "CV": "CV - 航空母艦",
-            "CVE": "CVE - 護衛空母",
-            "CVL": "CVL - 軽空母",
-            "CVS": "CVS - 対潜空母",
-            "SV": "SV - 飛行艇母艦",
-
-            # 揚陸艦艇
-            "LCSL": "LCSL - 上陸支援艇",
-
-            # 小型艦艇（哨戒・砲艦など）
-            "PC": "PC - 哨戒艇、駆潜艇",
-            "PT": "PT - 高速魚雷艇",
-            "FF": "FF - フリゲート",
-            "K": "K - コルベット",
-            "MB": "MB - ミサイル艇",
-            "PF": "PF - 哨戒フリゲート",
-            "PG": "PG - 砲艦",
-            "TB": "TB - 魚雷艇",
-
-            # 駆逐艦系
-            "D": "D - 水雷駆逐艦",
-            "DB": "DB - 通報艦",
-            "DD": "DD - 駆逐艦",
-            "DDE": "DDE - 対潜護衛駆逐艦",
-            "DDG": "DDG - ミサイル駆逐艦",
-            "DDR": "DDR - レーダーピケット駆逐艦",
-            "DE": "DE - 護衛駆逐艦",
-            "DL": "DL - 嚮導駆逐艦",
-            "DM": "DM - 敷設駆逐艦",
-            "DMS": "DMS - 掃海駆逐艦",
-            "DDH": "DDH - ヘリコプター搭載護衛艦",
-
-            # 潜水艦系
-            "CSS": "CSS - 沿岸潜水艦",
-            "MSM": "MSM - 特殊潜航艇",
-            "SC": "SC - 巡洋潜水艦",
-            "SCV": "SCV - 潜水空母",
-            "SF": "SF - 艦隊型潜水艦",
-            "SM": "SM - 敷設型潜水艦",
-            "SS": "SS - 航洋型潜水艦",
-
-            # 巡洋艦系
-            "ACR": "ACR - 装甲巡洋艦",
-            "C": "C - 防護巡洋艦",
-            "CA": "CA - 重巡・一等巡洋艦",
-            "CL": "CL - 軽巡洋艦/二等巡洋艦",
-            "CB": "CB - 大型巡洋艦",
-            "CF": "CF - 航空巡洋艦",
-            "CG": "CG - ミサイル巡洋艦",
-            "CM": "CM - 敷設巡洋艦",
-            "CS": "CS - 偵察巡洋艦",
-            "HTC": "HTC - 重雷装巡洋艦",
-            "TC": "TC - 水雷巡洋艦",
-            "TCL": "TCL - 練習巡洋艦",
-
-            # 戦艦系
-            "B": "B - 前弩級戦艦",
-            "BB": "BB - 戦艦",
-            "BBG": "BBG - ミサイル戦艦",
-            "BC": "BC - 巡洋戦艦",
-            "BF": "BF - 航空戦艦",
-            "BM": "BM - モニター艦",
-            "FBB": "FBB - 高速戦艦",
-            "PB": "PB - ポケット戦艦",
-            "SB": "SB - 超戦艦",
-            "CDB": "CDB - 海防戦艦",
-
-            # その他装甲艦
-            "IC": "IC - 装甲艦",
-            
-            # 特設・巡視船・その他艦艇
-            "AAA": "AAA - 特設防空艦",
-            "AAG": "AAG - 特設防空警備艦",
-            "AAM": "AAM - 特設掃海艇",
-            "AAS": "AAS - 特設駆潜艇",
-            "AAV": "AAV - 特設水上機母艦",
-            "AC": "AC - 特設巡洋艦",
-            "AG": "AG - 特設砲艦",
-            "AMS": "AMS - 特設敷設艦",
-            "APC": "APC - 特設監視艇",
-            "APS": "APS - 特設哨戒艦",
-            "CAM": "CAM - CAMシップ",
-            "MAC": "MAC - 特設空母",
-            "APB": "APB - 航行可能な宿泊艦",
-            "PL": "PL - 大型巡視船",
-            "PLH": "PLH - ヘリ搭載型",
-            "PM": "PM - 中型巡視船",
-            "WHEC": "WHEC - 長距離カッター"
-        }
 
         # 艦種をコンボボックスに追加
         for value in ship_type_mapping.values():
